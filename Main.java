3d6e02b5c88fc957a3f9b30875859e84935b4a41
milanparajuli0322 <milanparajuli0322@gmail.com>
2026-09-15 20:53:48 +0545

    feat : Added patient entity

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hospital manager");
+        Patient patient=new Patient("milan", 20,"male");
+        System.out.println("Hospital manager"); 
+        System.out.println(patient);
     }
 }
---
+++ Patient.java
@@ -0,0 +1,12 @@
+public class Patient {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Patient(String name,int age,String gender)
+    {
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
+}
