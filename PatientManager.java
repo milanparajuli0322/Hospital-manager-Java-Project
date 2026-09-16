fb56ebca017a280e971fb50f61d21026d58b2cdd
milanparajuli0322 <milanparajuli0322@gmail.com>
2026-09-16 21:32:14 +0545

    feat : Created patientmanagement class

---
+++ PatientManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public class PatientManager {
+    private ArrayList<Patient> patients;
+
+    
+    public PatientManager()
+    {
+        patients=new ArrayList<>();
+    }
+}
